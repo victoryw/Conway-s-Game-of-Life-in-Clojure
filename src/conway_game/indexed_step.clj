486c1d63f3847ec7feff42e2016d4b6663070218
tianyawy <tianya_wy@hotmail.com>
2016-06-19 23:52:15 +0800

    [victoryw] refactor:remove the loop

--- src/conway_game/indexed_step.clj
+++ src/conway_game/indexed_step.clj
@@ -2,18 +2,18 @@
   (:require [conway-game.count-neighbours :refer :all]))
 
   (defn indexed-step
-    ""
     [board]
     (let [w (count board)
           h (count (first board))]
-          (loop [new-board board x 0 y 0]
-              (cond
-                (>= x w) new-board
-                (>= y h) (recur new-board (inc x) 0)
-                :else
-                  (let [new-liveness
-                          (case (count-neighbours board [x y])
-                                2 (get-in board [x y])
-                                3 :on
-                                nil )]
-                    (recur (assoc-in new-board [x y] new-liveness) x (inc y)))))))
+        (reduce
+            (fn [new-board x]
+              (reduce
+                  (fn [new-board y]
+                    (let [new-liveness
+                            (case (count-neighbours board [x y])
+                              2 (get-in board [x y])
+                              3 :on
+                              nil)]
+                     (assoc-in new-board [x y] new-liveness)))
+                new-board (range h)))
+         board (range w))))
